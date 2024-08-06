da0f0ec840b335ebb5d418e10debed1e09c4c307
Ayushi Parmar <ayushi.parmar@1rivet.com>
2024-08-06 15:27:30 +0530

    feat: changed skeleton ui format

--- frontend/src/shared/components/SkeletonUi.tsx
+++ frontend/src/shared/components/SkeletonUi.tsx
@@ -9,9 +9,13 @@ export default function SkeletonUi() {
       p={12}
       style={{ border: "1px solid #e7ebe9", borderRadius: "8px" }}
     >
-      <Skeleton height={200} mt={6} width={"40%"} radius={"md"} />
-      <Skeleton height={20} mt={10} width={"60%"} radius={"md"} />
-      <Skeleton height={20} mt={6} width={"40%"} radius={"md"} />
+      <Flex mb={15} w={"100%"} align={"center"} justify={"space-between"}>
+        <Skeleton height={18} width={"30%"} radius={"xs"} />
+        <Skeleton height={18} width={"8%"} radius={"xs"} />
+      </Flex>
+      <Skeleton height={160} mt={8} width={"55%"} radius={"md"} />
+      <Skeleton height={15} mt={18} width={"80%"} radius={"sm"} />
+      <Skeleton height={15} mt={8} width={"40%"} radius={"sm"} />
     </Flex>
   );
 }
